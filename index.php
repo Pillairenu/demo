eaea9217f52725e48ec25a613bada9a61a8a5083
Pillairenu <113142647+Pillairenu@users.noreply.github.com>
2023-09-10 09:22:02 -0700

    Created an array and implemented for loop to print all values

--- index.php
+++ index.php
@@ -3,25 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <title>demo</title>
-    <style>
-        body
-            {
-                display:grid;
-                place-items:center;
-                height: 100vh;
-                margin:0;
-                font-family: sans-serif;
-            }
-    </style>
+    
 </head>
 <body>
 
        <h1>Recommended Books</h1>
 
+
+       <?php
+       
+       $books=[
+
+        "Do Androids Dream of Electric Sheep",
+        "The Langoliers",
+        "Hail Mary"
+
+       ];
+       
+       
+       
+       ?>
+
        <ul>
-        <li> Do Androids Dream of Electric Sheep </li>
-        <li> The Langoliers</li>
-        <li> Hail Mary</li>
+          <?php foreach ($books as $book)
+          {
+            echo "<li> $book </li>";
+          }
+          
+          ?>
        </ul>
 </body>
 </html>
