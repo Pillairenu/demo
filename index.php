6c89b3bc461e639fdfe3f9f52da9227bbd16ece7
Pillairenu <113142647+Pillairenu@users.noreply.github.com>
2023-09-09 22:52:51 -0700

    Added a PHP script

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
     <title>demo</title>
 </head>
 <body>
-<h1>Hello World</h1>
+    <h1>
+        
+        <?php 
+        
+          echo "Hello, World";
+        ?>
+
+
+    </h1>
 </body>
-</html> 
+</html>
