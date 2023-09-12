137b1aaece00923eec16bd6cb040293d40915281
Pillairenu <113142647+Pillairenu@users.noreply.github.com>
2023-09-11 22:59:59 -0700

    Implemented dynamic active navigation styling

--- index.php
+++ index.php
@@ -1,18 +1,9 @@
 <?php
+
+require "functions.php";
+
 $heading='Home';
 
-function dd($value)
-{
 
-      echo "<pre>";
-
-      var_dump($value);
-      
-      echo "</pre>";
-      die();
-}
-
-
-
-      require "views/index.view.php";
-       ?>
+require "views/index.view.php";
+?>
