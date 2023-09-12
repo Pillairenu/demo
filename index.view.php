3c738b3edaf52023201f31e50562cd9fb92d6dec
Pillairenu <113142647+Pillairenu@users.noreply.github.com>
2023-09-11 20:48:28 -0700

    Include a content

--- index.view.php
+++ index.view.php
@@ -127,9 +127,11 @@
       <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
     </div>
   </header>
+  
   <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
       <!-- Your content -->
+      <p> Hello.Welcome to the home page. </p>
     </div>
   </main>
 </div>
