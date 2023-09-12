9d54f59b0f3139abb29296a8e0c2d44ac8a7e6c8
Pillairenu <113142647+Pillairenu@users.noreply.github.com>
2023-09-11 21:01:41 -0700

    Include the about page

--- about.view.php
+++ about.view.php
@@ -130,7 +130,7 @@
   <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
       <!-- Your content -->
-      <p> Hello.Welcome to the home page. </p>
+      <p> Now you are on the About page. </p>
     </div>
   </main>
 </div>
